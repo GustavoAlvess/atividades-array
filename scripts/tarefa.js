80778e8a2d6fd4a44ccbe0afa67c35d8f8081ae8
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 08:55:30 -0300

    feat: Adiciona novas tarefas

--- scripts/tarefa.js
+++ scripts/tarefa.js
@@ -1,8 +1,10 @@
+// etapa 1 
+
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
 
 console.log(tarefas);
 
-tarefas.push('jogar videogame' , ' limpar pc')
+tarefas.push('jogar videogame' , ' limpar pc') // adicionando ao array
 
 console.log(tarefas);
 
