180f6bdb5cdf028fa9cc7423fe80e9d71e849acd
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 08:53:29 -0300

    feat: adiciona conteúdo a arquivo tarefas.js na pasta scripts

--- scripts/tarefa.js
+++ scripts/tarefa.js
@@ -1,2 +1,9 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+console.log(tarefas);
+
+tarefas.push('jogar videogame' , ' limpar pc')
+
+console.log(tarefas);
 
 
